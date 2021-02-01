b18a3b775b982ca53f2114c24b2248bf1d9e4cbd
Kosta S <43343162+KostaS10@users.noreply.github.com>
2021-02-01 23:20:42 +0100

    Create configureServerbackup.sh

---
+++ configureServerbackup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
+git clone https://github.com/KostaS10/notejam.git
+cd notejam/express/notejam/
+npm install
+node db.js
+sudo npm start
