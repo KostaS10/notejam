0ea4087afb975a22890c123348ffe9ee02821dd2
Kosta S <43343162+KostaS10@users.noreply.github.com>
2021-02-01 15:20:07 +0100

    Update configureServer.sh

--- configureServer.sh
+++ configureServer.sh
@@ -7,4 +7,18 @@ git clone https://github.com/KostaS10/notejam.git
 cd notejam/express/notejam/
 npm install
 node db.js
-sudo npm start
+sudo npm install -g pm2
+pm2 start bin/www
+pm2 startup systemd
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u nordcloud --hp /home/nordcloud
+sudo apt-get install -y nginx
+rm -rf /etc/nginx/sites-available/default
+cp default /etc/nginx/sites-available/default
+sudo nginx -t
+sudo systemctl restart nginx
+sudo su
+mkdir /etc/systemd/system/nginx.service.d
+printf "[Service]\nExecStartPost=/bin/sleep 0.1\n" > /etc/systemd/system/nginx.service.d/override.conf
+systemctl daemon-reload
+systemctl restart nginx 
+reboot
